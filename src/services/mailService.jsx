0f0ec815e96a41069b3f27fedf694aa35b54fa03
wassii-khan <wassiikhan933@gmail.com>
2025-06-27 22:12:51 +0500

    feat: Implement email sending functionality in SendMail service

--- src/services/mailService.jsx
+++ src/services/mailService.jsx
@@ -1,4 +1,27 @@
+import emailjs from "@emailjs/browser";
+
+const serviceId = import.meta.env.VITE_EMAIL_SERVICE_ID;
+const templateId = import.meta.env.VITE_TEMPLATE_ID;
+const apiKey = import.meta.env.VITE_EMAIL_API;
+
 // Send Mail Service
-export const SendMail = () => {
-  console.log("Mail Sent to the user.");
+export const SendMail = async (fullName, email, subject, message) => {
+  try {
+    const response = emailjs.send(
+      serviceId,
+      templateId,
+      {
+        fullName,
+        email,
+        subject,
+        message,
+      },
+      apiKey
+    );
+    console.log("email response", response);
+    return response;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email. Please try again later.");
+  }
 };
